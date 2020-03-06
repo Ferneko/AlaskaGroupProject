43c633e6450bda2e056a220d1121a2efd1a2d31a
rg.marsico@yahoo.com <rg.marsico@yahoo.com>
2020-03-06 14:36:07 -0300

    teste

---
+++ ProjetoJederson/WebApi/Model/Acompanhamento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Model
+{
+    public class Acompanhamento
+    {
+       public long id { get; set; }
+
+       public string nome { get; set; }
+
+       public string descricao { get; set; }
+
+       public decimal valor { get; set; }
+
+    }
+}
