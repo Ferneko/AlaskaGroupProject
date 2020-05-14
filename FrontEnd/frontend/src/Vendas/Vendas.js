0439243a31d683c9fe53950a144541b0cae40023
Fernando I5 <fernandocesar226@hotmail.com>
2020-05-14 09:00:10 -0300

    Teste de git

---
+++ FrontEnd/frontend/src/Vendas/Vendas.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Layout from '../Layout/Layout'
+
+export default class extends Component
+{
+    render(){
+        return (
+            <Layout>
+                <h1>Tela de vendas</h1>
+            </Layout>
+        )
+    }
+}
