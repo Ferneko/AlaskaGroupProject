78c53317a302e47b6a04905e13404ffb3bbaa40d
Fernando I5 <fernandocesar226@hotmail.com>
2020-05-14 17:10:31 -0300

    adicionando lista de vendas

---
+++ FrontEnd/frontend/src/Vendas/ListaVendas.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Layout from '../Layout/Layout'
+
+export default class ListaVendas extends Component
+{
+    render(){
+        return (
+            <Layout>
+                <h1>Lista de vendas</h1>
+              
+            </Layout>
+        )
+    }
+}
