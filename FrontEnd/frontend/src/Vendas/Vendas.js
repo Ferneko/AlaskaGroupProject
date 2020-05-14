9cab28fafddb4e8a859f6f0fbebb857163fecae3
Fernando I5 <fernandocesar226@hotmail.com>
2020-05-14 09:22:06 -0300

    Branch de teste

--- FrontEnd/frontend/src/Vendas/Vendas.js
+++ FrontEnd/frontend/src/Vendas/Vendas.js
@@ -7,6 +7,7 @@ export default class extends Component
         return (
             <Layout>
                 <h1>Tela de vendas</h1>
+                <h4>teste com a branch no code</h4>
             </Layout>
         )
     }
