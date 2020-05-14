280872044dae02ede614bb7c40582db4f89f202b
Fernando I5 <fernandocesar226@hotmail.com>
2020-05-14 17:24:37 -0300

    teste na branch teste-aula

--- FrontEnd/frontend/src/Vendas/Vendas.js
+++ FrontEnd/frontend/src/Vendas/Vendas.js
@@ -13,6 +13,10 @@ export default class Vendas extends Component
                 <p>
                     Testando o git pull
                 </p>
+
+                <p>
+                   teste na branch teste-aula
+                </p>
             </Layout>
         )
     }
