a54b5b701a638dfe2419e9b60f9cc34351f17414
Rafael Gabriel <rg.marsico@gmail.com>
2020-05-22 16:38:48 -0300

    teste

--- FrontEnd/frontend/src/Acompanhamentos/ListaAcompanhamentos.js
+++ FrontEnd/frontend/src/Acompanhamentos/ListaAcompanhamentos.js
@@ -1,142 +1,200 @@
-import React, { Component } from 'react';
-import Layout from '../Layout/Layout';
-import {Link} from 'react-router-dom';
-import Conexao from '../Conexao/Conexao';
+import React, { Component } from "react";
+import Layout from "../Layout/Layout";
+import { Link } from "react-router-dom";
+import Conexao from "../Conexao/Conexao";
 
 export default class ListaAcompanhamentos extends Component {
-    
-    constructor(props) {
-        super(props)
-        this.state = {
-            ListaAcompanhamentos: [],
-            query : "",
-            erro : null
+  constructor(props) {
+    super(props);
+    this.state = {
+      ListaAcompanhamentos: [],
+      query: "",
+      erro: null,
+    };
+    this.pesquisar = this.pesquisar.bind(this);
+    this.delete = this.delete.bind(this);
+    this.atualizaQuery = this.atualizaQuery.bind(this);
+  }
+
+  atualizaQuery(e) {
+    this.setState({ query: e.target.value });
+  }
+
+  componentDidMount() {
+    Conexao.get("/Acompanhamentos").then((resposta) => {
+      const dados = resposta.data;
+      if (dados.erro != null) {
+        this.setState({ erro: dados.erro });
+      } else {
+        this.setState({ ListaAcompanhamentos: dados });
+      }
+    });
+  }
+
+  pesquisar(e) {
+    console.log(this.state.query);
+    var data = this.state.query;
+    Conexao.get("/Acompanhamentos/PesquisarAcompanhamentos/" + data).then(
+      (resposta) => {
+        const dados = resposta.data;
+        if (dados.erro != null) {
+          this.setState({ erro: dados.erro });
+        } else {
+          this.setState({ ListaAcompanhamentos: dados });
         }
-        this.pesquisar = this.pesquisar.bind(this);
-        this.delete = this.delete.bind(this);
-        this.atualizaQuery = this.atualizaQuery.bind(this);
-    }
+      }
+    );
+  }
 
-    atualizaQuery(e){
-        this.setState({ query : e.target.value });
-    }
+  delete(e) {
+    Conexao.delete("/Acompanhamentos", {
+      params: { id: e.target.dataset.objeto },
+    }).then((resposta) => {
+      console.log(resposta.data);
+      const dados = resposta.data;
+      if (dados.erro != null) {
+        this.setState({ erro: dados.erro });
+      } else {
+        this.setState({ ListaAcompanhamentos: dados });
+      }
+    });
+    Conexao.get("/Acompanhamentos").then((resposta) => {
+      const dados = resposta.data;
+      if (dados.erro != null) {
+        this.setState({ erro: dados.erro });
+      } else {
+        this.setState({ ListaAcompanhamentos: dados });
+      }
+    });
+  }
 
-    componentDidMount() {
-        Conexao.get("/Acompanhamentos").then(resposta => {
-            const dados = resposta.data;
-            if(dados.erro != null){
-                this.setState({ erro : dados.erro });
-            }else{
-                this.setState({ ListaAcompanhamentos : dados });
-            }
-        });
-    }
+  render() {
+    return (
+      <Layout>
+        {this.state.erro != null ? (
+          <div
+            className="alert alert-danger alert-dismissible fade show"
+            role="alert"
+          >
+            {this.state.erro}
+            <button
+              type="button"
+              onClick={() => this.setState({ erro: null })}
+              className="close"
+              data-dismiss="alert"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+        ) : (
+          ""
+        )}
 
-    pesquisar(e){
-        console.log(this.state.query)
-        var data = this.state.query
-        Conexao.get("/Acompanhamentos/PesquisarAcompanhamentos/"+data).then(resposta => {
-            const dados = resposta.data;
-            if(dados.erro != null){
-                this.setState({ erro : dados.erro });
-            }else{
-                this.setState({ ListaAcompanhamentos : dados });
-            }
-        });
-    }
-
-    delete(e){
-        Conexao.delete("/Acompanhamentos", {params: { id: e.target.dataset.objeto }}).then(resposta => {
-            console.log(resposta.data)
-            const dados = resposta.data;
-            if(dados.erro != null){
-                this.setState({ erro : dados.erro });
-            }else{
-                this.setState({ ListaAcompanhamentos : dados });
-            }
-        });
-        Conexao.get("/Acompanhamentos").then(resposta => {
-            const dados = resposta.data;
-            if(dados.erro != null){
-                this.setState({ erro : dados.erro });
-            }else{
-                this.setState({ ListaAcompanhamentos : dados });
-            }
-        });
-    }
-
-    render() {
-        return (
-        <Layout>
-                {this.state.erro != null ?
-                    <div className="alert alert-danger alert-dismissible fade show" role="alert">
-                        {this.state.erro}
-                        <button type="button" onClick={() => this.setState({ erro: null })} className="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    </div>
-                : ""}
-
+        <div className="row">
+          <div className="col-12">
+            <div className="card">
+              <div className="card-header">Pesquisar</div>
+              <div className="card-body">
                 <div className="row">
-                    <div className="col-12">
-                        <div className="card">
-                            <div className="card-header">
-                                Pesquisar
-                            </div>
-                            <div className="card-body">
-                                <div className="row">
-                                    <div className="col-2">
-                                        <Link to="/CadastroAcompanhamento" className="btn btn-success">Novo Acompanhamento</Link>
-                                    </div>
-                                    <div className="col-10">
-                                        <div className="input-group">
-                                            <input type="text" className="form-control" placeholder="Digite aqui sua pesquisa" onChange={this.atualizaQuery} aria-label="Digite aqui sua pesquisa" aria-describedby="basic-addon2" />
-                                            <div className="input-group-append">
-                                                <button className="btn btn-primary" type="button" onClick={this.pesquisar}>Pesquisar</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                  <div className="col-2">
+                    <Link
+                      to="/CadastroAcompanhamento"
+                      className="btn btn-success"
+                    >
+                      Novo Acompanhamento
+                    </Link>
+                  </div>
+                  <div className="col-10">
+                    <div className="input-group">
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Digite aqui sua pesquisa"
+                        onChange={this.atualizaQuery}
+                        aria-label="Digite aqui sua pesquisa"
+                        aria-describedby="basic-addon2"
+                      />
+                      <div className="input-group-append">
+                        <button
+                          className="btn btn-primary"
+                          type="button"
+                          onClick={this.pesquisar}
+                        >
+                          Pesquisar
+                        </button>
+                      </div>
                     </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br />
+        <div className="row">
+          <div className="col-12">
+            <div className="card">
+              <div className="card-body">
+                <table className="table table-houver">
+                  <thead>
+                    <tr>
+                      <th>Id</th>
+                      <th>Imagem</th>
+                      <th>Nome</th>
+                      <th>Descrição</th>
+                      <th>Valor</th>
+                      <th>Ativo</th>
+                      <th></th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {this.state.ListaAcompanhamentos.map((item) => (
+                      <tr key={item.id}>
+                        <td>{item.id}</td>
+                        <td>{item.imagem}</td>
+                        <td>{item.nome}</td>
+                        <td>{item.descricao}</td>
+                        <td>{item.valor}</td>
+                        <td>
+                          <input
+                            disabled
+                            type="checkbox"
+                            defaultChecked={item.ativo}
+                          />
+                        </td>
 
-                </div>
-                <br />
-                <div className="row">
-                    <div className="col-12">
-                        <div className="card">
-                            <div className="card-body">
-                                <table className="table table-houver">
-                                    <thead>
-                                        <tr>
-                                            <th>Id</th>
-                                            <th>Imagem</th>
-                                            <th>Nome</th>
-                                            <th>Descrição</th>
-                                            <th>Valor</th>
-                                            <th>Ativo</th>
-                                            <th></th>
-                                            <th></th>
-                                        </tr>
-                                    </thead>
-                                <tbody>
-                                    {this.state.ListaAcompanhamentos.map((item) =>
-                                        <tr key={item.id}>
-                                            <td>{item.imagem}</td>
-                                            <td>{item.nome}</td>
-                                            <td>{item.descricao}</td>
-                                            <td>{item.valor}</td>
-                                            <td><input disabled type="checkbox" defaultChecked={item.ativo}/></td>
-                                            <td><button className="btn btn-warning">Editar</button></td>
-                                            <td><button className="btn btn-danger" onClick={this.delete} data-objeto={item.id}>Excluir</button></td>
-                                        </tr>
-                                    )}
-                                </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </Layout>);
-    }
-    
+                        <td>
+                          <Link
+                            key={item.id}
+                            to={{
+                              pathname: "/EditarAcompanhamento/" + item.id,
+                            }}
+                            className="btn btn-warning"
+                          >
+                            Editar
+                          </Link>
+                        </td>
+
+                        <td>
+                          <button
+                            className="btn btn-danger"
+                            onClick={this.delete}
+                            data-objeto={item.id}
+                          >
+                            Excluir
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    );
+  }
 }
--- FrontEnd/frontend/src/Layout/Layout.js
+++ FrontEnd/frontend/src/Layout/Layout.js
@@ -2,57 +2,44 @@ import React, { Component } from 'react'
 import './Layout.css';
 import { Link } from "react-router-dom";
 
-
-
 export default class extends Component {
     
     render() {
         return (<div>
             <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-                <div className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Sorveteria Alaska</div>
-              
+                <div className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Alaska Sorveteria</div>
                 <ul className="navbar-nav px-3">
                     <li className="nav-item text-nowrap">
                         <div className="nav-link" href="#">Sair do sistema</div>
                     </li>
                 </ul>
             </nav>
-
             <div className="container-fluid">
                 <div className="row">
                     <nav className="col-md-2 d-none d-md-block bg-light sidebar">
                         <div className="sidebar-sticky">
                             <ul className="nav flex-column">
-                                
                                 <li className="nav-item">
                                     <Link to="/ListaAcompanhamentos" className="nav-link" style={{ cursor: 'pointer' }}> <span data-feather="layers"></span>Acompanhamentos</Link>
                                 </li>
-
                                 <li className="nav-item">   
                                     <Link to="/ListaAdicional" className="nav-link" style={{ cursor: 'pointer' }}> <span data-feather="layers"></span>Adicionais</Link>      
                                 </li>
-
                                 <li className="nav-item">
                                     <Link to="/ListaSabores" className="nav-link" style={{ cursor: 'pointer' }}> <span data-feather="layers"></span>Sabores</Link>
                                 </li>
-
                                 <li className="nav-item">
                                     <Link to="/ListaClientes" className="nav-link" style={{ cursor: 'pointer' }}> <span data-feather="layers"></span>Clientes</Link>
                                 </li>
-                                
                                 <li className="nav-item">
                                     <Link to="/ListaUsuarios" className="nav-link" style={{ cursor: 'pointer' }}> <span data-feather="layers"></span>Usuários</Link>
                                 </li>
-
                                 <li className="nav-item">   
                                     <Link to="/ListaCasquinha" className="nav-link" style={{ cursor: 'pointer' }}> <span data-feather="layers"></span>Casquinha</Link>      
                                 </li>
-                             
                             </ul>
-
                         </div>
                     </nav>
-
                     <main role="main" className="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
                         {this.props.children}
                     </main>
